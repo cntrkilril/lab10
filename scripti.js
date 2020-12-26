93dc96858842f9db3c9b8afe3efae71af86b2767
Kirill Shalnov <shalkir2002@mail.ru>
2020-12-26 14:46:19 +0300

    Сделал поле для игры и кнопки

--- scripti.js
+++ scripti.js
@@ -7,3 +7,9 @@ function Cancel() {
     i=0
     document.getElementById("sch1").innerHTML=i;
 }
+    var area = document.getElementById("area"),
+    ctx = area.getContext("2d");
+    ctx.fillStyle="#FF0000";
+    ctx.fillRect(0, 0, area.width, area.height);
+    ctx.moveTo(50, 50);
+ctx.lineTo(150, 50);
